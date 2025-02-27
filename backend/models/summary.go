85d1168e8b0b43ec79f83474560e9186a3fad3b8
Rafiputraaa <raafiputraa2@gmail.com>
2025-02-27 14:24:06 +0700

    fix: add relation for summary

--- backend/models/summary.go
+++ backend/models/summary.go
@@ -9,11 +9,11 @@ import (
 type Summary struct {
 	ID			uint		`gorm:"primaryKey"`
 	ReportID  	uint      	`gorm:"uniqueIndex" json:"report_id"`
-	WorkerID		uint		`gorm:"not null" json:"admin_id"`
+	WorkerID	uint		`gorm:"not null" json:"admin_id"`
 	Summary		string		`gorm:"not null" json:"summary"` 
-	CreatedAt time.Time 	`json:"created_at"`
-	Worker *User `gorm:"foreignKey:WokerID" json:"worker"`
-	Report Report `gorm:"foreignKey:ReportID" json:"report"`
+	CreatedAt 	time.Time 	`json:"created_at"`
+	Worker		*User 		`gorm:"foreignKey:WorkerID" json:"worker"`
+	Report 		Report		`gorm:"foreignKey:ReportID" json:"report"`
 }
 
 func (Summary) TableName() string {
