07da569c3d91257e3a452d0cd77337eb0062447c
GUCC <gucc@webgucc.gunadarma.ac.id>
2025-08-11 15:09:17 +0700

    file data

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/n',
+  assetPrefix: '/n', // Optional but useful for static assets
+  trailingSlash: true, // Optional: makes URLs like `/n/about/` (not `/n/about`)
+
+};
 
 export default nextConfig;
